bf196aea9544af4c1394175b951209275ea40e5f
Ilya Bovbel <boff.ilya@gmail.com>
2017-03-09 14:18:56 +0300

    общая функция чтобы не загрязнять глобал замыкание

--- htask1/app.js
+++ htask1/app.js
@@ -1,29 +1,31 @@
-
-var word1 = prompt("Введите 1 слово:", "полиндром?");
-var word2 = prompt("Введите 2 слово:", reverse("полиндром?"));
-
-
-if (word1 == reverse(word2) )
+(function ()
 {
-	alert ("Эй, ты ввел полиндромы!");
-}
-else
-{
-	result = isAnagram (word1, word2);
-	alert (result);
-}
+	var word1 = prompt("Введите 1 слово:", "полиндром?");
+	var word2 = prompt("Введите 2 слово:", reverse("полиндром?"));
 
-function reverse(s)
-{
-    return s.split("").reverse().join("");
-}
 
-function sort (str)
-{
-	return str.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
-}
+	if (word1 == reverse(word2) )
+	{
+		alert ("Эй, ты ввел полиндромы!");
+	}
+	else
+	{
+		result = isAnagram (word1, word2);
+		alert (result);
+	}
 
-function isAnagram (original, test)
-{
-	return original.trim() === test.trim() ? false : sort(original) === sort(test);
-}
+	function reverse(s)
+	{
+	    return s.split("").reverse().join("");
+	}
+
+	function sort (str)
+	{
+		return str.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+	}
+
+	function isAnagram (original, test)
+	{
+		return original.trim() === test.trim() ? false : sort(original) === sort(test);
+	}
+})();
