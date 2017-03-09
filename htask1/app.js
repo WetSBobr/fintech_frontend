1a5fc6712c3c2d3ccaa495485a44f9ed31b3769c
Ilya Bovbel <boff.ilya@gmail.com>
2017-03-09 14:06:19 +0300

    htask1 - done

---
+++ htask1/app.js
@@ -0,0 +1,29 @@
+
+var word1 = prompt("Введите 1 слово:", "полиндром?");
+var word2 = prompt("Введите 2 слово:", reverse("полиндром?"));
+
+
+if (word1 == reverse(word2) )
+{
+	alert ("Эй, ты ввел полиндромы!");
+}
+else
+{
+	result = isAnagram (word1, word2);
+	alert (result);
+}
+
+function reverse(s)
+{
+    return s.split("").reverse().join("");
+}
+
+function sort (str)
+{
+	return str.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+}
+
+function isAnagram (original, test)
+{
+	return original.trim() === test.trim() ? false : sort(original) === sort(test);
+}
